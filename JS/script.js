0b22f44bb0b296128c8edc9fd7f89b8573c57fec
Fraser Mclennan <frasermclennan1302@gmail.com>
2023-04-21 21:00:23 +0100

    Final Version
    This is the final version of the project

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+function openSide() {
+  document.getElementById('pop-menu').style.width="250px";
+}
+
+function closeSide() {
+  document.getElementById('pop-menu').style.width="0px";
+
+}
+
+function openOption(evt, cityName){
+  var i,  miniNavContent, miniNavOptions;//Counter in the for loop
+ //declares the content
+  //declares links
+
+  miniNavContent = document.getElementsByClassName('miniNavContent');
+  for(i = 0; i < miniNavContent.length; i++){
+    miniNavContent[i].style.display = "none";
+  }
+
+  miniNavOptions = document.getElementsByClassName('miniNavOptions');
+  for (i = 0; i < miniNavOptions.length; i++) {
+    miniNavOptions[i].className = miniNavOptions[i].className.replace(" active", "");
+  }
+
+document.getElementById(cityName).style.display = "block";
+evt.currentTarget.className += " active";
+}
